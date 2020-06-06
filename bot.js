d772ccc1e62310a8aa8aa7929bf46b835d2298af
STOCAA <66504148+STOCAA@users.noreply.github.com>
2020-06-05 20:47:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '!' //prefix
+
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setStatus('ONLINE')
+    client.user.setPresence({
+        game: {
+            name: 'Premier Members',
+            type: 2
+        }
+    });
 });
 
 client.on('message', message => {
-    if (message.content === 'What is premier?') {
-    	message.reply('For information on premier, please read #『🤝』partners');
+    if (message.content === 'ping') {
+    	message.reply('Pong.');
   	}
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 

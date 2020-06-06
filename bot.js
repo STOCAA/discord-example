e43d31d1a367a04b3a0c8c5f500995d8c762eddb
STOCAA <66504148+STOCAA@users.noreply.github.com>
2020-06-05 20:45:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'What is premier?') {
+    	message.reply('For information on premier, please read #『🤝』partners');
   	}
 });
 

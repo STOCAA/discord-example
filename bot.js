bac0942611c594e5e1f5037a80e9064d815113fe
STOCAA <66504148+STOCAA@users.noreply.github.com>
2020-06-10 12:44:51 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
     client.user.setStatus('ONLINE')
     client.user.setPresence({
         game: {
-            name: '8 Premier Members',
+            name: '9 Premier Members',
             type: 3
         }
     });
